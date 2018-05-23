0b326724e764ce004dab85872edf9a40d67000a6
Karmanov Dmitry <karmanno@gmail.com>
2018-05-23 17:52:29 +1000

    Added textLoader ref into MainController class.

--- src/com/karmanno/verificator/gui/MainController.java
+++ src/com/karmanno/verificator/gui/MainController.java
@@ -1,5 +1,6 @@
 package com.karmanno.verificator.gui;
 
+import com.karmanno.verificator.io.TextFileLoader;
 import com.karmanno.verificator.log.LogHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,6 +14,7 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     private LogHandler logHandler;
+    private TextFileLoader textFileLoader = new TextFileLoader();
 
     @FXML
     Button startVerificationButton;
@@ -25,6 +27,47 @@ public class MainController implements Initializable {
         System.out.println("Mouse clicked");
         startVerificationButton.setDisable(true);
         logHandler.log("Hi there!");
+    }
+
+    @FXML
+    public void handleOnLoadFileButtonClicked(MouseEvent mouseEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open text file with accounts");
+        File file = fileChooser.showOpenDialog(new Stage());
+
+        if(file != null) {
+            try {
+                ArrayList<User> models = textFileLoader.loadModels(file);
+                tableView.getColumns().clear();
+                TableColumn<Map, String> usernameColumn = new TableColumn<>("Username");
+                TableColumn<Map, String> passwordColumn = new TableColumn<>("Password");
+                TableColumn<Map, String> statusColumn = new TableColumn<>("Status");
+
+                usernameColumn.setCellFactory(new MapValueFactory("username"));
+                passwordColumn.setCellFactory(new MapValueFactory("password"));
+                statusColumn.setCellFactory(new MapValueFactory("status"));
+
+                tableView.getColumns().addAll(usernameColumn, passwordColumn, statusColumn);
+
+                tableView.getItems().clear();
+
+                for(User model : models) {
+                    Map<String, String> dataRow = new HashMap<>();
+                    dataRow.put("username", model.getUsername());
+                    dataRow.put("password", model.getPassword());
+                    dataRow.put("status", model.getUserStatus().toString());
+                    tableView.getItems().add(dataRow);
+                }
+
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error while opening file");
+                alert.setHeaderText("Something went wrong");
+
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+            }
+        }
     }
 
     @Override
