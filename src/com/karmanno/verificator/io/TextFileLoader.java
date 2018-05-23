8363ae9335dded5960ef823be96ef8d165648687
Karmanov Dmitry <karmanno@gmail.com>
2018-05-23 17:55:06 +1000

    Added additional condition in loader method, removed static modificator.

--- src/com/karmanno/verificator/io/TextFileLoader.java
+++ src/com/karmanno/verificator/io/TextFileLoader.java
@@ -3,18 +3,22 @@ package com.karmanno.verificator.io;
 import com.karmanno.verificator.model.User;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class TextFileLoader {
-    public static ArrayList<User> loadModels(String filename) throws IOException {
+    public ArrayList<User> loadModels(File file) throws Exception {
         ArrayList<User> users = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader("file.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader(file));
         String line = reader.readLine();
 
         while (line != null) {
             String[] credentials = line.split(" ");
+
+            if(credentials.length < 2)
+                throw new RuntimeException("File has wrong format");
+
             users.add(new User(credentials[0], credentials[1]));
             line = reader.readLine();
         }
