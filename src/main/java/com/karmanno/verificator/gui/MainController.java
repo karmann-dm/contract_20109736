3cb4bbe8a6303b01fedf8dd2a1e0aef35fcbb89f
karmann-dm <karmanno@gmail.com>
2018-05-24 12:51:27 +1000

    Added some button handling, started verification worker code.

--- src/main/java/com/karmanno/verificator/gui/MainController.java
+++ src/main/java/com/karmanno/verificator/gui/MainController.java
@@ -4,26 +4,28 @@ import com.karmanno.verificator.io.TextFileLoader;
 import com.karmanno.verificator.log.LogHandler;
 import com.karmanno.verificator.model.User;
 import com.karmanno.verificator.model.UserStatus;
-import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class MainController implements Initializable {
     private LogHandler logHandler;
     private TextFileLoader textFileLoader = new TextFileLoader();
+    private AlertFactory alertFactory = new AlertFactory();
+    ArrayList<User> models = null;
 
     @FXML
     Button startVerificationButton;
@@ -38,6 +40,9 @@ public class MainController implements Initializable {
     Button loadTextFileButton;
 
     @FXML
+    Button exportVerifiedContactsButton;
+
+    @FXML
     TableView tableView;
 
     @FXML
@@ -47,16 +52,40 @@ public class MainController implements Initializable {
         return new Task() {
             @Override
             protected Object call() throws Exception {
+                int index = 0;
+                for(User model : models) {
+                    model.setUserStatus(UserStatus.VERIFYING);
+                    tableView.getItems().set(index, model);
 
+                    WebDriver webDriver = new ChromeDriver();
+                    webDriver.get("https://www.nike.com/gb/en_gb/gb/");
+                    webDriver.close();
+                    //Thread.sleep(1000);
 
+                    model.setUserStatus(UserStatus.VERIFIED);
+                    tableView.getItems().set(index, model);
+                    updateProgress(index, models.size());
 
-                return null;
+                    index++;
+                }
+
+                return new Object();
             }
         };
     }
 
     @FXML
     public void handleOnStartVerificationClicked(MouseEvent mouseEvent) {
+        if(models == null) {
+            alertFactory.createAlert(
+                    "Error",
+                    "There are no accounts to verify",
+                    Alert.AlertType.WARNING,
+                    "There are no accounts to verify"
+            ).showAndWait();
+            return;
+        }
+
         logHandler.log("Verification started");
         startVerificationButton.setDisable(true);
         Task task = createWorker();
@@ -65,6 +94,7 @@ public class MainController implements Initializable {
             progressBar.progressProperty().unbind();
             progressBar.setProgress(1.0);
             startVerificationButton.setDisable(false);
+            logHandler.log("Verification ended");
         });
         task.setOnFailed(e -> task.getException().printStackTrace());
         Thread thread = new Thread(task);
@@ -76,18 +106,27 @@ public class MainController implements Initializable {
         logHandler.log("Clearing table...");
         tableView.getItems().clear();
         tableView.getColumns().clear();
+        if(models != null) {
+            models.clear();
+        }
         logHandler.log("Table is clear");
     }
 
     @FXML
     public void handleOnLoadFileButtonClicked(MouseEvent mouseEvent) {
+        logHandler.log("Loading started...");
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open text file with accounts");
         File file = fileChooser.showOpenDialog(new Stage());
 
         if(file != null) {
             try {
-                ArrayList<User> models = textFileLoader.loadModels(file);
+                if(models != null)
+                    models.clear();
+
+                models = textFileLoader.loadModels(file);
+
                 tableView.getColumns().clear();
 
                 TableColumn usernameColumn = new TableColumn("Username");
@@ -103,12 +142,60 @@ public class MainController implements Initializable {
                     tableView.getItems().add(model);
                 }
             } catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error while opening file");
-                alert.setHeaderText("Something went wrong");
+                alertFactory.createAlert(
+                        "Error while opening file",
+                        "Something went wrong",
+                        Alert.AlertType.ERROR, e.getMessage()
+                ).showAndWait();
+            }
+            finally {
+                logHandler.log("Loading ended, loaded " + (models == null ? "0" : models.size()) + " entries");
+            }
+        }
+        else {
+            logHandler.log("File is null");
+        }
+    }
 
-                alert.setContentText(e.getMessage());
-                alert.showAndWait();
+    @FXML
+    public void handleOnExportVerifiedContactsButtonClicked(MouseEvent mouseEvent) {
+        if(models == null) {
+            alertFactory.createAlert(
+                    "Error",
+                    "There are no accounts to export",
+                    Alert.AlertType.WARNING,
+                    "There are no accounts to export"
+            ).showAndWait();
+            return;
+        }
+        if(models.stream().filter(user -> user.getUserStatus().equals(UserStatus.VERIFIED)).count() == 0) {
+            alertFactory.createAlert(
+                    "Error",
+                    "There are no verified accounts",
+                    Alert.AlertType.WARNING,
+                    "There are no verified accounts"
+            ).showAndWait();
+            return;
+        }
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Export verified contacts");
+        fileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt"));
+        File file = fileChooser.showSaveDialog(new Stage());
+
+        if(file != null) {
+            try {
+                textFileLoader.saveModels(
+                        file,
+                        models.stream().filter(
+                                user -> user.getUserStatus().equals(UserStatus.VERIFIED)
+                        ).collect(Collectors.toList()));
+            } catch (Exception e) {
+                alertFactory.createAlert(
+                        "Error while opening file",
+                        "Something went wrong",
+                        Alert.AlertType.ERROR, e.getMessage()
+                ).showAndWait();
             }
         }
     }
