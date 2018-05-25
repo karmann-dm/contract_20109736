09d707ed674a5ba11888738b409895613ff2a6fb
karmann-dm <karmanno@gmail.com>
2018-05-25 18:27:54 +1000

    Implemented automatic navigation methods

--- src/main/java/com/karmanno/verificator/automation/ChromeNikeAutomation.java
+++ src/main/java/com/karmanno/verificator/automation/ChromeNikeAutomation.java
@@ -1,59 +1,76 @@
 package com.karmanno.verificator.automation;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import javax.swing.*;
-
 public class ChromeNikeAutomation implements NikeAutomation {
-    private static final String address = "www.nike.com/gb/en_gb/";
+    private static final String address = "https://www.nike.com/gb/en_gb/";
     private WebDriver webDriver;
     private Actions builder;
 
-    public ChromeNikeAutomation(ChromeDriver chromeDriver) {
+    public ChromeNikeAutomation(WebDriver chromeDriver) {
         setWebDriver(chromeDriver);
     }
 
-    public void setWebDriver(ChromeDriver webDriver) {
+    public void setWebDriver(WebDriver webDriver) {
         this.webDriver = webDriver;
         builder = new Actions(webDriver);
     }
 
-    public ChromeDriver getWebDriver() {
-        return (ChromeDriver)webDriver;
+    public WebDriver getWebDriver() {
+        return webDriver;
     }
 
     @Override
-    public void get() {
+    public void get() throws InterruptedException {
         webDriver.get(address);
+        Thread.sleep(4000);
     }
 
     @Override
-    public void refresh() {
+    public void refresh() throws InterruptedException {
         webDriver.navigate().refresh();
+        Thread.sleep(5000);
     }
 
     @Override
-    public void clickLogin() {
+    public void clickLogin() throws InterruptedException {
         WebElement loginButton = webDriver.findElement(By.cssSelector("span[class='login-text']"));
-        builder.moveToElement(loginButton, 0, 0).click().build().perform();
+        builder.moveToElement(loginButton).click().build().perform();
+        Thread.sleep(5000);
     }
 
     @Override
-    public void clickAcceptCookies() {
-
+    public void clickAcceptCookies() throws InterruptedException {
+        WebElement acceptButton = webDriver.findElement(By.cssSelector("button[class='nsg-button nsg-grad--nike-orange yes-button cookie-settings-button js-yes-button']"));
+        builder.moveToElement(acceptButton, 0, 0).click().build().perform();
+        Thread.sleep(5000);
     }
 
     @Override
-    public void dismissChangeCountryAlert() {
-
+    public void dismissChangeCountryAlert() throws InterruptedException {
+        clickLogin();
     }
 
     @Override
-    public void fulfillForm(String username, String password) {
+    public void fulfillForm(String username, String password) throws InterruptedException {
+        WebElement usernameInput = webDriver.findElement(By.cssSelector("input[type='email']"));
+        WebElement passwordInput = webDriver.findElement(By.cssSelector("input[type='password']"));
 
+        usernameInput.click();
+        usernameInput.clear();
+        usernameInput.sendKeys(username);
+
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+
+        passwordInput.sendKeys(Keys.ENTER);
+
+        Thread.sleep(1000);
     }
 }
